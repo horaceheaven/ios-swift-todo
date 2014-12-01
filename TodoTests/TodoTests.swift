4d44934dd90a2265db0c2699d436c4f75e934ab4
horaceheaven <hheaven@medullan.com>
2014-12-01 15:04:13 -0400

    Added one more test for experimentation

--- TodoTests/TodoTests.swift
+++ TodoTests/TodoTests.swift
@@ -26,6 +26,11 @@ class TodoTests: XCTestCase {
         XCTAssert(true, "Pass")
     }
     
+    func testExampleANextOne() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measureBlock() {
